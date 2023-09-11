8f8a1ebec816ca824676ba49670ee779ecb7a5b3
GowthamAC <acgowtham123@gmail.com>
2023-09-11 20:49:51 -0300

    common

--- HMS.ZEOMEDI/src/test/java/Interview/a1.java
+++ HMS.ZEOMEDI/src/test/java/Interview/a1.java
@@ -1,22 +1,7 @@
  package Interview;
 
-
 class a1{
 	
 public static void main(String[] args) {
-	int a=-1;
-	int i=2;
-	int count=0;
-	while(i<a/2){
-	    if(a%i==0){
-	        count++;
-	    }
-	    i++;
-	}
-	if(count==0){
-	    System.out.println(a+" is a prime number");
-	}else{
-	    System.out.println(a+" not is a prime number");
-	}
 }
 }
